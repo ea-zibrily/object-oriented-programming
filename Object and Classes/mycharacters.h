21722c29bdc6340595a506effc0a40eb4b7f2384
Zibrrr <zyuuannn@gmail.com>
2022-09-08 19:33:23 +0700

    revisi class 1

---
+++ Object and Classes/mycharacters.h
@@ -0,0 +1,19 @@
+#pragma once
+
+class snyder
+{
+private:
+    int age;
+    int eyes;
+    int leg;
+    int nameIndex;
+
+public:
+    void setAge(int);
+
+    void setEyes(int);
+
+    void setLeg(int);
+
+    void setName(int);
+};
